be7247e8a1f5ab8a71e98eb7dad0404c6d13b4c0
Orxan-Ibrahimov <orxan.ibrahimli.98@gmail.com>
2024-07-22 22:10:06 +0400

    LikerListSForPostLogicHandled

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -29,7 +29,9 @@ export class PostsComponent implements OnInit {
   like_id: string;
   liked: boolean;
   emotions: Emotion[];
-  diff_emotions: Emotion[] = [];
+  de: Emotion[];
+  likes: Like[][];
+
   emotion: Emotion;
   default: Emotion;
   like: Like;
@@ -42,30 +44,44 @@ export class PostsComponent implements OnInit {
       this.me = me;
       this.refresh_like_id(this.Post.likes);
       this.likeService.get().subscribe((likes) => {
+        // this.de = this.CountLikerEmotionList(this.Post.likes);
+        // console.log(this.Post.likes);
+        
+        
+
         likes.forEach((like) => {
           if (like.post?.id === this.Post?.id && like.who_likes?.id == me?.id) {
             this.like = like;
             this.emotion = like.emotion;
-            let has_emotion = true;
-            if (this.diff_emotions.length === 0)
-              this.diff_emotions.push(like.emotion);
-            else {
-              this.diff_emotions.forEach((emotion) => {
-                if (like.emotion.image !== emotion.image) has_emotion = false;
-              });
-
-              if (!has_emotion) {
-                this.diff_emotions.push(like.emotion);
-                has_emotion = true;
-              }
-            }
           }
         });
       });
     });
     this.emotion_service.get_emotions().subscribe((emotions) => {
       this.emotions = emotions;
+      this.likes = this.OKB(this.Post.likes);
+      // console.log('tr:',this.OKB(this.Post.likes));
+
     });
+  }
+
+  OKB(likes: Like[]) {
+    let new_likes:any[] = [];
+
+    for (let index = 0; index < this.emotions.length; index++) {
+      const emotion = this.emotions[index];
+      let new_sub_likes:any[] = [];
+      for (let index = 0; index < likes.length; index++) {
+        const like = likes[index];
+        if (like.emotion.image === emotion.image)
+          new_sub_likes.push(like);
+      }
+      new_likes.push(new_sub_likes);
+    }
+
+    console.log(new_likes);
+    
+    return new_likes;
   }
 
   DoLike(event: MouseEvent, emotion: Emotion | null) {
@@ -86,6 +102,7 @@ export class PostsComponent implements OnInit {
           this.Post = p;
           this.liked = true;
           this.like = createdLike;
+          this.likes = this.OKB(this.Post.likes);
 
           this.refresh_like_id(this.Post.likes);
         });
@@ -103,6 +120,7 @@ export class PostsComponent implements OnInit {
           this.postService.getPostById(this.Post.id).subscribe((p) => {
             this.Post = p;
             this.refresh_like_id(this.Post.likes);
+            this.likes = this.OKB(this.Post.likes);
           });
         });
     } else {
@@ -115,6 +133,29 @@ export class PostsComponent implements OnInit {
     }
   }
 
+  CountLikerEmotionList(Likes: Like[]) {
+    let diff_emotions: Emotion[] = [];
+    Likes.forEach((like) => {
+      let has_emotion = false;
+      if (diff_emotions.length === 0) diff_emotions.push(like.emotion);
+      else {
+        for (let index = 0; index < diff_emotions.length; index++) {
+          const emotion = diff_emotions[index];
+          if (like.emotion.image === emotion.image) {
+            has_emotion = true;
+            break;
+          }
+        }
+
+        if (!has_emotion) {
+          diff_emotions.push(like.emotion);
+          has_emotion = false;
+        }
+      }
+    });
+
+    return diff_emotions;
+  }
   OpenComments() {
     this.Post.read_comments = !this.Post.read_comments;
     this.postService.update_current_post(this.Post);
